6e7776b253c176314846536a1f2367bde769d5b0
Vandita Bharti <kajalk0306@gmail.com>
2025-10-31 00:41:41 +0530

    update backend frontend admin url

--- frontend_p/src/Context/ShopContext.jsx
+++ frontend_p/src/Context/ShopContext.jsx
@@ -17,12 +17,12 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(()=>{
-    fetch('http://localhost:4000/allproducts')
+    fetch('https://trendytrove-new.onrender.com/allproducts')
     .then((response)=>response.json())
     .then((data)=>setAll_Product(data))
 
     if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/getcart',{
+      fetch('https://trendytrove-new.onrender.com/getcart',{
         method:'POST',
         headers:{
           Accept:'application/form-data',
@@ -40,7 +40,7 @@ const ShopContextProvider = (props) => {
   const addToGlamBin = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/addtocart',{
+      fetch('https://trendytrove-new.onrender.com/addtocart',{
         method:'POST',
         headers:{
           Accept:'application/form-data',
@@ -60,7 +60,7 @@ const ShopContextProvider = (props) => {
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     if(localStorage.getItem('auth-token')){
-      fetch('http://localhost:4000/removefromcart',{
+      fetch('https://trendytrove-new.onrender.com/removefromcart',{
         method:'POST',
         headers:{
           Accept:'application/form-data',
